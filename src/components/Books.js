a6a83448fd42ae31ad7b49b05970966e10dd8f8a
anuj chauhan <cchaanuj@amazon.com>
2023-02-21 01:06:16 +0530

    adding nav and booklist

---
+++ src/components/Books.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Books() {
+  return (
+    <div>
+        
+            <li> never loose faith</li>
+            <li>its all in your head</li>
+            <li>shoe dog</li>
+      
+    </div>
+  )
+}
+
+export default Books
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Navbar.css'
+
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+        <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>contact</li>
+
+            </ul>
+        </nav>
+
+
+      
+    </div>
+  )
+}
+
+export default Navbar
